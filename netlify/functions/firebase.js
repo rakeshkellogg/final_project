e1323b888c77f0bf6dc2640cf1f9e34b5cae73b8
Alex Glover <alex.glover@kellogg.northwestern.edu>
2021-03-11 20:58:29 -0600

    add firebase authenticator

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,15 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+    apiKey: "AIzaSyASc6MEnufSxR67UMGIBLmV42_v8UEldZQ",
+    authDomain: "kiei-final-project.firebaseapp.com",
+    projectId: "kiei-final-project",
+    storageBucket: "kiei-final-project.appspot.com",
+    messagingSenderId: "895011058032",
+    appId: "1:895011058032:web:101efae9adb7e3a7ecdfb5",
+    measurementId: "G-PM7Z2L5KMY"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
