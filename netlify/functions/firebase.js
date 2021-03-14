94be2daf5a961cd59fa300714c267e73fd7df553
Alex Glover <alex.glover@kellogg.northwestern.edu>
2021-03-14 14:29:02 -0500

    re-setup firebase

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -2,13 +2,13 @@ const firebase = require("firebase/app")
 require("firebase/firestore")
 
 const firebaseConfig = {
-    apiKey: "AIzaSyASc6MEnufSxR67UMGIBLmV42_v8UEldZQ",
-    authDomain: "kiei-final-project.firebaseapp.com",
-    projectId: "kiei-final-project",
-    storageBucket: "kiei-final-project.appspot.com",
-    messagingSenderId: "895011058032",
-    appId: "1:895011058032:web:101efae9adb7e3a7ecdfb5",
-    measurementId: "G-PM7Z2L5KMY"
+  apiKey: "AIzaSyASc6MEnufSxR67UMGIBLmV42_v8UEldZQ",
+  authDomain: "kiei-final-project.firebaseapp.com",
+  projectId: "kiei-final-project",
+  storageBucket: "kiei-final-project.appspot.com",
+  messagingSenderId: "895011058032",
+  appId: "1:895011058032:web:101efae9adb7e3a7ecdfb5",
+  measurementId: "G-PM7Z2L5KMY"
 } // replace test test
 
 if (!firebase.apps.length) {
